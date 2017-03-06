abe265e6d2bc2cff4ac0bc8dde97fd342943951f
Blak3Nick <blaken2021@gmail.com>
2017-03-06 13:49:31 -0500

    Creating custom routes

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,28 +14,42 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie( ) { Name = "Shrekked" };
-            return View(movie);
-        }
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Cust1"},
+                new Customer {Name = "Cust2"}
+            };
+            var viewModel = new RandomMovieViewModel
+                {
+                 Movie   = movie,
+                 Customers = customers
+                };
             
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex = {0}&sortBy={1}", pageIndex, sortBy));
-
+            return View(viewModel);
         }
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content( year + "/" + month);
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id=" + id);
+        //}
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+            
+        //        pageIndex = 1;
+
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+        //    return Content(String.Format("pageIndex = {0}&sortBy={1}", pageIndex, sortBy));
+
+        //}
+        //[Route("movies/released/{year}/{month:regex(\\d{2}:range(1, 12)}")]
+        //public ActionResult ByReleaseYear(int year, int month)
+        //{
+        //    return Content( year + "/" + month);
+        //}
+        
     }
 
 }
